89156d6cc1c939fc2771ce5b85448c4ddf0340f9
Hpsaturn <hpsaturn@gmail.com>
2024-11-09 11:11:15 +0100

    added jpgdelay for slow display receivers

--- lib/preferences/preferences-keys.h
+++ lib/preferences/preferences-keys.h
@@ -1,6 +1,6 @@
 #define CONFIG_KEYS_LIST   \
   X(KJPGM, "jpgmode", INT) \
   X(KJPGQ, "jpgqlty", INT) \
+  X(KJPGD, "jpgwait", INT) \
   X(KSETUP, "setup", BOOL) \
   X(KCOUNT, "KCOUNT", UNKNOWN)
-  
--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,14 @@ const char* app_prompt   = "camcli";
 const char* setup_prompt = "setup";
 
 int setup_mode = 0;
-bool internal_jpg = true;
+bool jpgmode = true;
 bool debug = false;
+int jpgwait = 60;
 
 void processFrameNoJPG() {
   if (Camera.get()) {
     radio.sendData(Camera.fb->buf, Camera.fb->len);
-    delay(50);  // ==> weird delay when you are using only DRAM.
+    delay(jpgwait);  // ==> weird delay when you are using only DRAM.
     if (debug) printFPS("CAM:");
     Camera.free();
   }
@@ -39,6 +40,7 @@ void processFrameJPG() {
     size_t out_jpg_len = 0;
     frame2jpg(Camera.fb, Camera.config.jpeg_quality, &out_jpg, &out_jpg_len);
     radio.sendData(out_jpg, out_jpg_len);
+    delay(jpgwait);  // ==> weird delay when you are using only DRAM.
     if (debug) printFPS("CAM:");
     free(out_jpg);
     Camera.free();
@@ -46,8 +48,9 @@ void processFrameJPG() {
 }
 
 void configCameraNoJPG() {
-  // Configuration without using the PSRAM, only DRAM.
+  // Configuration without PSRAM, only DRAM.
   // (faster and better quality)
+  Camera.config.frame_size = FRAMESIZE_240X240;
   Camera.config.pixel_format = PIXFORMAT_JPEG;
   Camera.config.fb_location = CAMERA_FB_IN_DRAM;
 }
@@ -55,6 +58,7 @@ void configCameraNoJPG() {
 void configCameraJPG() {
   // Configuration using the PSRAM, external JPG processing.
   // (less RAM and improved transfer size)
+  Camera.config.frame_size = FRAMESIZE_240X240;
   Camera.config.pixel_format = PIXFORMAT_RGB565;
   Camera.config.fb_location = CAMERA_FB_IN_PSRAM;
 }
@@ -73,8 +77,9 @@ void cameraInit() {
 
 void printSettings(Stream *response){
   response->println();
-  response->printf("jpeg internal\t: %s\r\n",internal_jpg ? "enable" : "disable");
+  response->printf("jpeg internal\t: %s\r\n",jpgmode ? "enable" : "disable");
   response->printf("jpeg quality \t: %i\r\n",Camera.config.jpeg_quality);
+  response->printf("jpeg delay   \t: %i\r\n",jpgwait);
   response->printf("fbuffer count\t: %i\r\n",Camera.config.fb_count);
   response->printf("frame size   \t: %i\r\n",Camera.config.frame_size);
 }
@@ -84,13 +89,14 @@ void info(char *args, Stream *response) {
   printSettings(response);
 }
 
-void jpgmode(char *args, Stream *response) {
+void set_jpgm(char *args, Stream *response) {
   if (!setup_mode){
     response->println("This setting only is possible in setup mode");
     return;
   }
-  internal_jpg = !internal_jpg; 
-  if (internal_jpg) {
+  jpgmode = !jpgmode;
+  cfg.saveBool(PKEYS::KJPGM, jpgmode);
+  if (jpgmode) {
     response->println("Internal JPG ON");
   }
   else {
@@ -101,6 +107,14 @@ void jpgmode(char *args, Stream *response) {
 void set_jpgq(char *args, Stream *response) {
   Pair<String, String> operands = wcli.parseCommand(args);
   Camera.config.jpeg_quality = operands.first().toInt();
+}
+
+void set_jpgd(char *args, Stream *response) {
+  Pair<String, String> operands = wcli.parseCommand(args);
+  long x = (long)(operands.first().toDouble());
+  jpgwait = map(x, 50, 100, 50, 100);
+  response->printf("internal jpg decoding delay: %i\r\n", jpgwait);
+  cfg.saveInt(PKEYS::KJPGD,jpgwait);
 }
 
 void enable_debug(char *args, Stream *response) {
@@ -137,9 +151,8 @@ void clear(char *args, Stream *response){
 
 void setup_init(char *args, Stream *response) {
   cfg.saveBool(PKEYS::KSETUP, true);
-
-  response->println("\r\nEntering to setup mode. Restarting..\r\n");
-  delay(1000);
+  response->println("\r\n\nEntering to setup mode. Restarting..\r\n");
+  delay(2000);
   reboot(nullptr,response);
 }
 
@@ -149,6 +162,8 @@ void setup_exit(char *args, Stream *response) {
 }
 
 void initCameraSetup() {
+  jpgmode = cfg.getBool(PKEYS::KJPGM, true);
+  jpgwait = cfg.getInt(PKEYS::KJPGD, 60);
   if (cfg.getBool(PKEYS::KSETUP, false)) {
     setup_mode=1;
     uint32_t start = millis();
@@ -161,7 +176,7 @@ void initCameraSetup() {
     Serial.println("\r\nSettings saved. Booting..\r\n");
   }
   wcli.loop();
-  if (internal_jpg)
+  if (jpgmode)
     configCameraNoJPG();
   else
     configCameraJPG();
@@ -169,15 +184,16 @@ void initCameraSetup() {
 
 void initSerialShell(){
   wcli.setSilentMode(true);  // less debug output
-  wcli.add("jpgmode",&jpgmode,        "\tinternal JPG mode");
-  wcli.add("jpgqlty",&set_jpgq,       "\tset JPG quality");
-  wcli.add("info",   &info,           "\t\tsystem status info");
-  wcli.add("debug",  &enable_debug,   "\t\ttoggle debug mode");
-  wcli.add("setup",  &setup_init,     "\t\tenter to safe mode camera setup");
-  wcli.add("exit",   &setup_exit,     "\t\tleave setup mode");
-  wcli.add("clear",  &clear,          "\t\tclear shell");
-  wcli.add("halt",   &halt,           "\t\tperform a ESP32 deep sleep");
-  wcli.add("reboot", &reboot,         "\tperform a ESP32 reboot");
+  wcli.add("jpgmode", &set_jpgm,       "\tinternal JPG mode");
+  wcli.add("jpgqlty", &set_jpgq,       "\tset JPG quality");
+  wcli.add("jpgwait", &set_jpgd,       "\tset delay for JPG decoding (default 60)");
+  wcli.add("info",    &info,           "\t\tsystem status info");
+  wcli.add("debug",   &enable_debug,   "\t\ttoggle debug mode");
+  wcli.add("setup",   &setup_init,     "\t\tenter to safe mode camera setup");
+  wcli.add("exit",    &setup_exit,     "\t\tleave setup mode");
+  wcli.add("clear",   &clear,          "\t\tclear shell");
+  wcli.add("halt",    &halt,           "\t\tperform a ESP32 deep sleep");
+  wcli.add("reboot",  &reboot,         "\tperform a ESP32 reboot");
   // wcli.shell->attachLogo(logo);
   wcli.begin(app_prompt);
 }
@@ -202,8 +218,10 @@ void setup() {
   initCameraSetup();
   cameraInit();
 
-  // Optional M5Core2 receiver B8:F0:09:C6:0E:CC
-  const uint8_t macRecv[6] = {0xB8,0xF0,0x09,0xC6,0x0E,0xCC};
+  // My M5Core2 receiver B8:F0:09:C6:0E:CC
+  // My CrowPanel receiver B0:81:84:74:18:2C
+  // const uint8_t macRecv[6] = {0xB8,0xF0,0x09,0xC6,0x0E,0xCC};
+  const uint8_t macRecv[6] = {0xB0,0x81,0x84,0x74,0x18,0x2C};
   radio.setTarget(macRecv);
   radio.init();
   
@@ -211,7 +229,7 @@ void setup() {
 }
 
 void loop() {
-  if (internal_jpg) processFrameNoJPG();
+  if (jpgmode) processFrameNoJPG();
   else processFrameJPG();
   btnB.tick();
   wcli.loop();
