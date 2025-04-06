035239af89dcf07708199031f54d414f2d83474f
Hpsaturn <hpsaturn@gmail.com>
2025-04-06 18:04:50 +0200

    new feature: channel using the last version ESPNowCam Library

--- lib/preferences/preferences-keys.h
+++ lib/preferences/preferences-keys.h
@@ -3,6 +3,7 @@
   X(KJPGQ,  "jpgqlty", INT) \
   X(KJPGD,  "jpgwait", INT) \
   X(KFSIZE, "fsize",   INT) \
+  X(KCHANL, "channel", INT) \
   X(KSETUP, "setup",   BOOL) \
   X(KTMAC,  "target",  STRING ) \
   X(KCOUNT, "KCOUNT",  UNKNOWN)
